289744378743734c62001adc4e151e0a8eabc2ac
poi <poi>
2020-05-30 01:24:00 +0800

    c

---
+++ reactapp/.umirc.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+  ], 
+  sass: {
+    implementation: require('node-sass'),
+  },
+  cssModulesTypescriptLoader: {}
+});
